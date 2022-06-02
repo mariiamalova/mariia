8c1151635d6077094cc8ccfcf0761935950ed546
mariiamalova <106603539+mariiamalova@users.noreply.github.com>
2022-06-02 23:14:14 +0300

    Add files via upload

---
+++ PP_1_1_3-4_JDBC_Hibernate-master/src/main/java/jm/task/core/jdbc/Main.java
@@ -0,0 +1,26 @@
+package jm.task.core.jdbc;
+
+
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
+import jm.task.core.jdbc.model.User;
+
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) throws SQLException {
+        UserDao userDao2 = new UserDaoHibernateImpl();
+        userDao2.createUsersTable();
+      // userDao2.dropUsersTable();
+        userDao2.saveUser("Name3", "LastName1", (byte) 20);
+        userDao2.saveUser("Name1121", "LastName1", (byte) 20);
+        userDao2.saveUser("Name1231", "LastName1", (byte) 20);
+        userDao2.saveUser("Name112", "LastName1", (byte) 20);
+        //userDao2.cleanUsersTable();
+       userDao2.getAllUsers();
+        for (User user : userDao2.getAllUsers()) {
+            System.out.println(user);
+        }
+
+    }
+}
